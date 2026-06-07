81db2df42f5fcd1c09f795827502d9ab56e8b12f
Ayush Bommana <bommanaayush07@gmail.com>
2026-06-07 22:02:40 +0530

    feat: Return error in all the endpoints

--- internal/repositories/users_repository.go
+++ internal/repositories/users_repository.go
@@ -2,16 +2,17 @@ package repositories
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/ayushWeb07/AirBnb-Go-Api-Gateway/internal/database/models"
 	"go.uber.org/zap"
 )
 
 type UserRepositoryInterface interface {
-	CreateUser()
-	GetUserById()
-	GetAllUsers()
-	DeleteUserById()
+	GetAllUsers() ([]*models.UserModel, error)
+	GetUserById() (*models.UserModel, error)
+	CreateUser() error
+	DeleteUserById() error
 }
 
 type UserRepository struct {
@@ -19,7 +20,7 @@ type UserRepository struct {
 	logger *zap.Logger
 }
 
-func (ur *UserRepository) GetAllUsers() {
+func (ur *UserRepository) GetAllUsers() ([]*models.UserModel, error) {
 	// create the dummy instance
 	var userModels []*models.UserModel
 
@@ -30,6 +31,8 @@ func (ur *UserRepository) GetAllUsers() {
 	if err != nil {
 		ur.logger.Error("Something went wrong while fetching all the users",
 			zap.String("error", err.Error()))
+
+		return nil, err
 	}
 	defer rows.Close()
 
@@ -41,7 +44,7 @@ func (ur *UserRepository) GetAllUsers() {
 			ur.logger.Error("Failed to fetch all the users from the database",
 				zap.String("error", err.Error()))
 
-			return
+			return nil, err
 		}
 
 		userModels = append(userModels, userModel)
@@ -51,14 +54,16 @@ func (ur *UserRepository) GetAllUsers() {
 		ur.logger.Error("Failed to fetch all the users from the database",
 			zap.String("error", err.Error()))
 
-		return
+		return nil, err
 	}
 
 	ur.logger.Info("Successfully fetched all the users from the database",
 		zap.Int("count", len(userModels)))
+
+	return userModels, nil
 }
 
-func (ur *UserRepository) GetUserById() {
+func (ur *UserRepository) GetUserById() (*models.UserModel, error) {
 	// create the dummy instance
 	userModel := &models.UserModel{}
 
@@ -70,13 +75,13 @@ func (ur *UserRepository) GetUserById() {
 			ur.logger.Error("Failed to fetch the user from the database",
 				zap.String("error", err.Error()))
 
-			return
+			return nil, err
 		}
 
 		ur.logger.Error("Failed to insert user into the database",
 			zap.String("error", err.Error()))
 
-		return
+		return nil, err
 	}
 
 	ur.logger.Info("Successfully fetched the user from the database",
@@ -84,9 +89,11 @@ func (ur *UserRepository) GetUserById() {
 		zap.String("user_username", userModel.Username),
 		zap.String("user_email", userModel.Email),
 	)
+
+	return userModel, nil
 }
 
-func (ur *UserRepository) CreateUser() {
+func (ur *UserRepository) CreateUser() error {
 	// create a dummy user model instance
 	userModel := &models.UserModel{
 		Username: "ayush",
@@ -101,7 +108,7 @@ func (ur *UserRepository) CreateUser() {
 		ur.logger.Error("Failed to insert user into the database",
 			zap.String("error", err.Error()))
 
-		return
+		return err
 	}
 
 	id, err := result.LastInsertId()
@@ -110,14 +117,16 @@ func (ur *UserRepository) CreateUser() {
 		ur.logger.Error("Failed to insert user into the database",
 			zap.String("error", err.Error()))
 
-		return
+		return err
 	}
 
 	ur.logger.Info("Successfully inserted user into the database",
 		zap.Int64("user_id", id))
+
+	return nil
 }
 
-func (ur *UserRepository) DeleteUserById() {
+func (ur *UserRepository) DeleteUserById() error {
 	// prepare and execute the query
 	query := "DELETE FROM users WHERE id = ?"
 	result, err := ur.db.Exec(query, 1)
@@ -126,7 +135,7 @@ func (ur *UserRepository) DeleteUserById() {
 		ur.logger.Error("Failed to delete user from the database",
 			zap.String("error", err.Error()))
 
-		return
+		return err
 	}
 
 	// check if any rows got affected
@@ -136,18 +145,20 @@ func (ur *UserRepository) DeleteUserById() {
 		ur.logger.Error("Failed to delete user from the database",
 			zap.String("error", err.Error()))
 
-		return
+		return err
 	}
 
 	if rowsAffected == 0 {
 		ur.logger.Error("No user has been deleted from the database",
 			zap.Int("user_id", 1))
 
-		return
+		return fmt.Errorf("No user has been deleted from the database")
 	}
 
 	ur.logger.Info("Successfully deleted the user from the database",
 		zap.Int("user_id", 1))
+
+	return nil
 }
 
 func NewUserRepository(logger *zap.Logger, db *sql.DB) UserRepositoryInterface {
